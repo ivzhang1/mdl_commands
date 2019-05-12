bd4401ef105d81f0d140483b8089fcdfa6890fb5
ivzhang1 <izhang1@stuy.edu>
2019-05-12 13:26:55 -0400

    fix bug with not making copies of arrays

--- script.py
+++ script.py
@@ -50,7 +50,7 @@ def run(filename):
 
     for command in commands:
         if command['op'] == 'push':
-            stack.append(stack[-1][:])
+            stack.append([row[:] for row in stack[-1]])
 
         elif command['op'] == 'pop':
             stack.pop()
@@ -58,29 +58,29 @@ def run(filename):
         elif command['op'] == 'move':
             t = make_translate(command['args'][0],command['args'][1],command['args'][2])
             matrix_mult(stack[-1], t)
-            stack[-1] = t
+            stack[-1] = [r[:] for r in t]
 
         elif command['op'] == 'scale':
             t = make_scale(command['args'][0],command['args'][1],command['args'][2])
             matrix_mult(stack[-1], t)
-            stack[-1] = t
+            stack[-1] = [r[:] for r in t]
 
         elif command['op'] == 'rotate':
             t = new_matrix()
             if(command['args'][0] == 'x'):
-                t = make_rotX(command['args'][1])
+                t = make_rotX(command['args'][1]*(math.pi / 180))
 
             elif(command['args'][0] == 'y'):
-                t = make_rotY(command['args'][1])
+                t = make_rotY(command['args'][1]*(math.pi / 180))
 
             elif(command['args'][0] == 'z'):
-                t = make_rotZ(command['args'][1])
+                t = make_rotZ(command['args'][1]*(math.pi / 180))
 
             else:
                 print("Input axis for rotate is incorrect")
 
             matrix_mult(stack[-1], t)
-            stack[-1] = t
+            stack[-1] = [r[:] for r in t]
 
         elif command['op'] in 'sphere box torus':
             if command['op'] == 'sphere':
@@ -91,11 +91,9 @@ def run(filename):
                 add_torus(polygons, float(command['args'][0]), float(command['args'][1]), float(command['args'][2]), float(command['args'][3]), float(command['args'][4]), step_3d)
 
             matrix_mult( stack[-1], polygons )
+
             if(command['constants']):
-                try:
-                    draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, command['constants'])
-                except KeyError:
-                    draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+                draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, command['constants'])
             else:
                 draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
             polygons = []
@@ -106,11 +104,14 @@ def run(filename):
             draw_lines(edges, screen, zbuffer, color)
             edges = []
 
+        elif command['op'] == 'constants':
+            pass
+
         elif command['op'] == 'save':
-            save_extension(screen, command['args'])
+            save_extension(screen, command['args'][0] + ".png")
 
         elif command['op'] == 'display':
             display(screen)
 
         else:
-            print("Unknown operator")
+            print("Unknown operator", command['op'])
